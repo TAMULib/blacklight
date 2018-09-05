f5a4ba6aa0ffa2cba9b9df97499a6b60b4247957
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2018-09-05 14:45:08 -0500

    Added collection title box if a collection is selected

--- lib/generators/blacklight/tamu/app/helpers/application_helper.rb
+++ lib/generators/blacklight/tamu/app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def link_to_url options = {}
     link_to options[:value][0], options[:value][0]
   end
+
+  def get_collection
+    if (params[:f] && params[:f][:collection])
+      return params[:f][:collection]
+    end
+  end
 end
